9152cd0e9de5a4fbb6e4e3bb3c340e2912315a22
CSP-Simon <caldaras_simon_paul@yahoo.com>
2022-02-20 22:00:08 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from flask import Flask, request, render_template, session, redirect,Response,send_file
+from flask import Flask, request, render_template, session, redirect, Response, send_file
 import numpy as np
 import pandas as pd
 import tabulate
@@ -9,45 +9,75 @@ import seaborn as sns
 
 app = Flask(__name__)
 nav = Navigation(app)
-Bdata=pd.read_csv('Breast Cancer.csv')
-Diabet=pd.read_csv('diabetes.csv')
-Mdata=pd.read_csv('Maternal Health Risk Data Set.csv')
+Bdata = pd.read_csv('Breast Cancer.csv')
+Diabet = pd.read_csv('diabetes.csv')
+Mdata = pd.read_csv('Maternal Health Risk Data Set.csv')
 
-#Data cleaning
+# Data cleaning
 
 Bdata.drop(['Unnamed: 32', 'id'], axis=1, inplace=True)
 Bdata.diagnosis.replace(['M', 'B'], [1, 0], inplace=True)
 
-
 nav.Bar('top', [
-    nav.Item('Breast Cancer data','table1'),
-    nav.Item('Diabetes data','table2'),
-    nav.Item('Maternal Health Risk Data','table3'),
+    nav.Item('Breast Cancer data', 'table1'),
+    nav.Item('Diabetes data', 'table2'),
+    nav.Item('Maternal Health Risk Data', 'table3'),
 ])
+
+
+class Table:
+
+    def __init__(self, data):
+        self.data = data
+
+    def table(self):
+        buf = io.StringIO()
+        self.data.info(buf=buf)
+        s = buf.getvalue()
+        df = pd.DataFrame(s.split("\n"), columns=['Data info'])
+        df2 = self.data.isna().sum()
+        df2 = pd.DataFrame(df2, columns=['Data null values'])
+
+        return df, df2
+
+
+class Correlation:
+    def __init__(self, data,x,y):
+        self.data = data
+        self.x = x
+        self.y = y
+
+    def cor(self):
+        fig, ax = plt.subplots(figsize=(self.x, self.y))
+        corr = self.data.corr()
+        corr.to_markdown()
+        lower_triang = corr.where(np.tril(np.ones(corr.shape)).astype(np.bool))
+        sns.heatmap(lower_triang, vmax=.8, square=True, cmap="BuPu", annot=True)
+        img = io.BytesIO()
+        fig.savefig(img, format='png')
+        img.seek(0)
+        return img
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 # Breast Cancer routes
 # ------------------------------------------------------------
 
 @app.route('/table1', methods=("POST", "GET"))
 def table1():
+    table = Table(Bdata)
+    df, df2 = table.table()
 
-    buf = io.StringIO()
-    Bdata.info(buf=buf)
-    s= buf.getvalue()
-    df=pd.DataFrame(s.split("\n"),columns=['Breast Cancer info'])
-    df2=Bdata.isna().sum()
-    df2=pd.DataFrame(df2,columns=['Breast Cancer null values'])
-
-    return render_template('Bdata.html', tables=[df.to_html(classes='data',index=False),df2.to_html(classes='data')], titles=df.columns.values)
-
+    return render_template('Bdata.html', tables=[df.to_html(classes='data', index=False), df2.to_html(classes='data')],
+                           titles=df.columns.values)
 
 
 @app.route('/plot1')
 def plot1():
-
     features_mean = list(Bdata.columns[1:11])
     # split dataframe into two based on diagnosis
     dfM = Bdata[Bdata['diagnosis'] == 1]
@@ -60,8 +90,9 @@ def plot1():
         ax.figure
         binwidth = (max(Bdata[features_mean[idx]]) - min(Bdata[features_mean[idx]])) / 50
         ax.hist([dfM[features_mean[idx]], dfB[features_mean[idx]]], bins=np.arange(min(Bdata[features_mean[idx]]),
-        max(Bdata[features_mean[idx]]) + binwidth, binwidth),
-        alpha=0.5, stacked=True, label=['M', 'B'], color=['r', 'g'])
+                                                                                   max(Bdata[features_mean[
+                                                                                       idx]]) + binwidth, binwidth),
+                alpha=0.5, stacked=True, label=['M', 'B'], color=['r', 'g'])
         ax.legend(loc='upper right')
         ax.set_title(features_mean[idx])
     img = io.BytesIO()
@@ -69,17 +100,13 @@ def plot1():
     img.seek(0)
     return send_file(img, mimetype='image/png')
 
+
 @app.route('/cor1')
 def cor1():
-    fig, ax = plt.subplots(figsize=(19,15))
-    corr=Bdata.corr()
-    corr.to_markdown()
-    lower_triang=corr.where(np.tril(np.ones(corr.shape)).astype(np.bool))
-    sns.heatmap(lower_triang,vmax=.8, square=True, cmap="BuPu",annot=True)
-    img = io.BytesIO()
-    fig.savefig(img, format='png')
-    img.seek(0)
+    correlation=Correlation(Bdata,19,15)
+    img=correlation.cor()
     return send_file(img, mimetype='image/png', cache_timeout=-1)
+
 
 # -------------------------------------------------------------------
 
@@ -88,17 +115,12 @@ def cor1():
 
 @app.route('/table2', methods=("POST", "GET"))
 def table2():
+    table = Table(Diabet)
+    df, df2 = table.table()
 
-    buf = io.StringIO()
-    Diabet.info(buf=buf)
-    s= buf.getvalue()
-    df = pd.DataFrame(s.split("\n"), columns=['Breast Cancer info'])
-    df2 = Diabet.isna().sum()
-    df2 = pd.DataFrame(df2, columns=['Breast Cancer null values'])
-
-    return render_template('Diabetes.html', tables=[df.to_html(classes='data', index=False), df2.to_html(classes='data')],
+    return render_template('Diabetes.html',
+                           tables=[df.to_html(classes='data', index=False), df2.to_html(classes='data')],
                            titles=df.columns.values)
-
 
 
 @app.route('/plot2')
@@ -112,73 +134,55 @@ def plot2():
     axes = axes.ravel()
     for idx, ax in enumerate(axes):
         ax.figure
-        ax.hist( Diabet[features_mean[idx]])
+        ax.hist(Diabet[features_mean[idx]])
         ax.set_title(features_mean[idx])
     img = io.BytesIO()
     fig.savefig(img, format='png')
     img.seek(0)
     return send_file(img, mimetype='image/png')
 
+
 @app.route('/cor2')
 def cor2():
-    fig, ax = plt.subplots(figsize=(12,10))
-    corr=Diabet.corr()
-    corr.to_markdown()
-    lower_triang=corr.where(np.tril(np.ones(corr.shape)).astype(np.bool))
-    sns.heatmap(lower_triang,vmax=.8, square=True, cmap="BuPu",annot=True)
-    img = io.BytesIO()
-    fig.savefig(img, format='png')
-    img.seek(0)
+    correlation=Correlation(Diabet,11,9)
+    img=correlation.cor()
     return send_file(img, mimetype='image/png', cache_timeout=-1)
 
 
 # Maternal Health Risk routes
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 
 @app.route('/table3', methods=("POST", "GET"))
 def table3():
-
-    buf = io.StringIO()
-    Mdata.info(buf=buf)
-    s= buf.getvalue()
-    df = pd.DataFrame(s.split("\n"), columns=['Breast Cancer info'])
-    df2 = Mdata.isna().sum()
-    df2 = pd.DataFrame(df2, columns=['Breast Cancer null values'])
+    table = Table(Mdata)
+    df, df2 = table.table()
 
     return render_template('Mdata.html', tables=[df.to_html(classes='data', index=False), df2.to_html(classes='data')],
                            titles=df.columns.values)
 
+
 @app.route('/plot3')
 def plot3():
-    fig, ax = plt.subplots()
-    sns.countplot(x = 'RiskLevel',data=Mdata)
+    fig, axes = plt.subplots(nrows=2,figsize=(10,15))
+    sns.countplot(x='RiskLevel', data=Mdata,ax=axes[0]).set(title="Number Of Patients In Each Risk Category")
+    sns.boxplot(x='RiskLevel', y='Age', data=Mdata, ax=axes[1]).set(title="Analysis In Age on Risk Level")
     img = io.BytesIO()
     fig.savefig(img, format='png')
     img.seek(0)
     return send_file(img, mimetype='image/png', cache_timeout=-1)
 
-@app.route('/boxplot3')
-def boxplot3():
-    fig, ax = plt.subplots()
-    sns.boxplot(x = 'RiskLevel',y='Age',data=Mdata)
-    img = io.BytesIO()
-    fig.savefig(img, format='png')
-    img.seek(0)
-    return send_file(img, mimetype='image/png', cache_timeout=-1)
+
+
 
 @app.route('/cor3')
 def cor3():
-    fig, ax = plt.subplots(figsize=(9,7))
-    corr=Mdata.corr()
-    corr.to_markdown()
-    lower_triang=corr.where(np.tril(np.ones(corr.shape)).astype(np.bool))
-    sns.heatmap(lower_triang,vmax=.8, square=True, cmap="BuPu",annot=True)
-    img = io.BytesIO()
-    fig.savefig(img, format='png')
-    img.seek(0)
+    correlation=Correlation(Mdata,10,9)
+    img=correlation.cor()
     return send_file(img, mimetype='image/png', cache_timeout=-1)
 
-#------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------
 
 if __name__ == '__main__':
     app.run()
